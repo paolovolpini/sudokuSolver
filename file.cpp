bbab305fe98333508b52ef332a8327cfd5b05a7c
Paolo Volpini <paolo.volpini4@gmail.com>
2024-09-22 20:02:00 +0200

    Created file.cpp

---
+++ file.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class sudokuSolver {
+    private:
+        int table[9][9];
+    public:
+
+
+};
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cerr << "Bad usage!\nUsage: " << argv[0] << " <filename>.txt" << endl;
+        return 1;
+    }
+    return 0;
+}
