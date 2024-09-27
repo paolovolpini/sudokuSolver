81e595c9c9cac9a80830618842f90e2cae333463
Paolo Volpini <paolo.volpini4@gmail.com>
2024-09-27 11:01:47 +0200

    Created class "SudokuSolver"

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,7 +1,17 @@
 import sys 
 
+class SudokuSolver:
+    def __init__(self):
+        self.board = [[0] * 9] * 9
+        print(self.board)
+    
+    def initializeFromFile(self, filename):
+        pass
+
 if __name__ == "__main__":
     if(len(sys.argv) != 2):
         print(f"{sys.argv[0]} must have <filename.txt> argument!")
         sys.exit(1)
+    solver = SudokuSolver()
+    solver.initializeFromFile(sys.argv[0])
 
